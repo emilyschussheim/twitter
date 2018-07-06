6d3b2ce2b1d9bc12d8cbedaf8648ec1dd18cef07
Emily Schussheim <eschuss@gmail.com>
2018-07-06 14:32:42 -0700

    scrolling

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -14,17 +14,23 @@
 #import "LoginViewController.h"
 #import "DetailsViewController.h"
 #import "ComposeViewController.h"
+#import "TweetProfileViewController.h"
+#import "InfiniteScrollActivityView.h"
 
 
-@interface TimelineViewController () <TweetCellDelegate, ComposeViewControllerDelegate, UITableViewDataSource, UITableViewDelegate>
+@interface TimelineViewController () <UIScrollViewDelegate, TweetCellDelegate, ComposeViewControllerDelegate, UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) IBOutlet UITableView *timelineTableView;
 @property (strong, nonatomic) NSArray *tweetArray;
 @property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property (assign, nonatomic) BOOL isMoreDataLoading;
 - (IBAction)logoutTapped:(id)sender;
 
 @end
 
 @implementation TimelineViewController
+
+bool isMoreDataLoading = false;
+InfiniteScrollActivityView* loadingMoreView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -38,10 +44,69 @@
     [self.refreshControl addTarget:self action:@selector(getTweets) forControlEvents:UIControlEventValueChanged];
     [self.timelineTableView insertSubview:self.refreshControl atIndex:0];
     
+    [self addInfiniteScrollActivityIndicator];
+    
+    
+}
+- (void) addInfiniteScrollActivityIndicator {
+    CGRect frame = CGRectMake(0, self.timelineTableView.contentSize.height, self.timelineTableView.bounds.size.width, InfiniteScrollActivityView.defaultHeight);
+    loadingMoreView = [[InfiniteScrollActivityView alloc] initWithFrame:frame];
+    loadingMoreView.hidden = true;
+    [self.timelineTableView addSubview:loadingMoreView];
+    
+    UIEdgeInsets insets = self.timelineTableView.contentInset;
+    insets.bottom += InfiniteScrollActivityView.defaultHeight;
+    self.timelineTableView.contentInset = insets;
+    
 }
 
 - (void)tweetCell:(TweetCell *)tweetCell didTap:(User *)user{
+    
     [self performSegueWithIdentifier:@"UsersProfileSegue" sender:user];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if(self.isMoreDataLoading == FALSE){
+     
+        int scrollViewContentHeight = self.timelineTableView.contentSize.height;
+        int scrollOffsetThreshold = scrollViewContentHeight - self.timelineTableView.bounds.size.height;
+        
+        //start requesting
+        if(scrollView.contentOffset.y > scrollOffsetThreshold && self.timelineTableView.isDragging) {
+                self.isMoreDataLoading = true;
+            
+                // Update position of loadingMoreView, and start loading indicator
+                CGRect frame = CGRectMake(0, self.timelineTableView.contentSize.height, self.timelineTableView.bounds.size.width, InfiniteScrollActivityView.defaultHeight);
+            loadingMoreView.frame = frame;
+            [loadingMoreView startAnimating];
+            
+            [self loadMoreData];
+        }
+    }
+}
+
+-(void)loadMoreData{
+    
+    // ... Create the NSURLRequest (myRequest) ...
+    
+    // Configure session so that completion handler is executed on main UI thread
+//    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+//
+//    NSURLSession *session  = [NSURLSession sessionWithConfiguration:configuration delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+//
+//    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *requestError) {
+//        if (requestError != nil) {
+//
+//        }
+//        else
+
+    [self getTweets];
+    
+    self.isMoreDataLoading = false;
+    [loadingMoreView stopAnimating];
+            
+    [self.timelineTableView reloadData];
+
 }
 
 - (void)getTweets {
@@ -85,8 +150,14 @@
         ComposeViewController *composeController = (ComposeViewController*)navigationController.topViewController;
         composeController.delegate = self;
     }
+    
+    if ([segue.identifier isEqualToString:@"UsersProfileSegue"]) {
 
+        TweetProfileViewController *viewController = [segue destinationViewController];
+        User *user = sender;
+        viewController.user = user;
 
+    }
 }
 
 
--- twitter/View Controllers/TweetProfileViewController.m
+++ twitter/View Controllers/TweetProfileViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "TweetProfileViewController.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface TweetProfileViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *propicImage;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tweetCount;
+@property (weak, nonatomic) IBOutlet UILabel *followersCount;
+@property (weak, nonatomic) IBOutlet UILabel *followingCount;
 
 @end
 
@@ -16,12 +23,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setProfileUI];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (void) setProfileUI {
+    
+    self.nameLabel.text = self.user.name;
+    self.usernameLabel.text = self.user.screenName;
+    self.tweetCount.text = self.user.tweetCount;
+    self.followersCount.text = self.user.followersCount;
+    self.followingCount.text = self.user.followingCount;
+    [self.propicImage setImageWithURL:self.user.propicURL];
+    
 }
 
 /*
